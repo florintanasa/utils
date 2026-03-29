bef28f9c21b53266443c960ced8032e183297cd7
Florin Tanasa <florin.tanasa@gmail.com>
2026-03-29 20:53:46 +0300

    feat: Update and enhance 'punte' Docker network configuration

--- create_docker_network.sh
+++ create_docker_network.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 docker network create \
-	--driver bridge \ # is a brigde
-	--subnet 172.18.1.0/24 \ # subnet with network address from 172.18.1.1 - 172.18.1.254, broadcast 172.18.1.255, mask 255.255.255.0
-	--ip-range 172.18.1.128/25 \ # ip range from 172.18.1.129 - 172.18.1.254 with GW 172.18.1.128
-	punte # network name
+    # is a brigde
+	--driver bridge \
+	# subnet with network address from 172.18.2.1 - 172.18.2.254, broadcast 172.18.2.255, mask 255.255.255.0
+	--subnet 172.18.2.0/24 \
+	# ip range from 172.18.2.129 - 172.18.2.254 with GW 172.18.2.128
+	--ip-range 172.18.2.128/25 \
+	# interface name for outside docker - used on firewall
+	-o "com.docker.network.bridge.name"="br_docker0" \
+	# network name on intern docker - used on containers
+	punte
 
